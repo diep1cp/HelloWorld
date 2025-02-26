bd40d959113b78aab9bdaaaec7eb8ca0431fdf4d
chidi <chidi@CHI_PC>
2025-02-26 10:30:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace HelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello world class demo!");
+            Console.ReadLine();
+
+            int x;
+            x = 3;
+        }
+    }
+}
